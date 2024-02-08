bfc8a37c00308c3d470136aec62b6b3137c2e0e0
vikramsamak <vikramsamak02@gmail.com>
2024-02-08 21:27:34 +0530

    UI Designing. Fixes #5

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
+import Home from "./pages/Home/Home"
+
 function App() {
 
   return (
-    <div>
-      App
+    <div className="p-4 h-screen flex items-center justify-center">
+      <Home />
     </div>
 
 
