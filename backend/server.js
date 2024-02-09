e503223949bfa842bd8d94704d1efea0a04a89da
vikramsamak <vikramsamak02@gmail.com>
2024-02-09 23:12:07 +0530

    Deployment Fixes #11

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,12 @@ import cookieParser from "cookie-parser";
 import connectToMongoDb from "./db/connectToMongoDb.js";
 import dotenv from "dotenv"
 import { app, server } from "./socket/socket.js";
+import path from "path"
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve()
 
 app.use(express.json());
 app.use(cookieParser());
@@ -18,8 +20,10 @@ app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 app.use("/api/users", userRoutes)
 
-app.get("/", (req, res) => {
-    res.send("Chat App!")
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
 })
 
 
